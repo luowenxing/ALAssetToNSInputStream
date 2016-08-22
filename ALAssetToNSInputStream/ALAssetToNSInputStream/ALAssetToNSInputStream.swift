03b372c28bcb61d4a95fb4a46c8a6bf24de34701
Luo <511352272@qq.com>
2016-08-22 10:20:49 +0800

    remove unnecessary files.

--- ALAssetToNSInputStream/ALAssetToNSInputStream/ALAssetToNSInputStream.swift
+++ ALAssetToNSInputStream/ALAssetToNSInputStream/ALAssetToNSInputStream.swift
@@ -92,6 +92,64 @@ class ALAssetToNSInputStream:NSObject,NSStreamDelegate {
     
 }
 
+class ALAssetNSInputStream:NSInputStream {
+    
+    
+    var rept:ALAssetRepresentation!
+    var lib:ALAssetsLibrary!
+    var status:NSStreamStatus = .NotOpen
+    private var offset:Int64 = 0
+    private var size:Int64 = 0
+    
+    convenience override init(URL url: NSURL) {
+        self.init()
+        if let lib = ALAsset.getLib(),asset = ALAsset.getAssetFromUrlSync(lib, url: url.absoluteString) {
+            self.lib = lib
+            self.rept = asset.defaultRepresentation()
+            self.size = self.rept.size()
+        }
+    }
+    
+    override func read(buffer: UnsafeMutablePointer<UInt8>, maxLength len: Int) -> Int {
+        self.status = .Reading
+        var error:NSError?
+        let readBytes = self.rept.getBytes(buffer, fromOffset: self.offset, length: len, error: &error)
+        if let err = error {
+            NSLog(err.localizedDescription)
+            self.close()
+        } else {
+            self.offset += readBytes
+        }
+        return readBytes
+    }
+    
+    override var hasBytesAvailable: Bool {
+        get {
+            return self.offset < self.size
+        }
+    }
+    
+    override func open() {
+        self.status = .Open
+    }
+    
+    
+    override func close() {
+        self.status = .Closed
+    }
+    
+    override var streamStatus: NSStreamStatus {
+        get {
+            if self.status != .Closed && self.offset >= self.size {
+                self.status = .AtEnd
+            }
+            return self.status
+        }
+    }
+    
+}
+
+
 
 
 extension ALAsset {
